caad03ffc774a8a117c36ee8075b0014d8e67a38
Sirojiddin Yakubov <yakubov9791999@gmail.com>
2023-01-07 14:28:22 +0500

    add kubernetes config files

--- client.py
+++ client.py
@@ -2,17 +2,16 @@ import os
 
 import grpc
 
-import auth_pb2, auth_pb2_grpc
+from grpc_generated_files import auth_pb2, auth_pb2_grpc
 
 
 class GatewayClient:
-    auth_host = os.environ.get('AUTH_HOST', 'localhost')
+    service = os.environ.get('AUTH_SVC_ADDRESS', 'localhost:9999')
 
-    def __init__(self, host=auth_host, server_port=9999):
-        self.host = host
-        self.server_port = server_port
+    def __init__(self, service=service):
+        self.service = service
 
-        self.channel = grpc.insecure_channel(f'{self.host}:{self.server_port}')
+        self.channel = grpc.insecure_channel(service)
         self.stub = auth_pb2_grpc.AuthServiceStub(self.channel)
 
     def register(self, username, password, email):
@@ -20,3 +19,17 @@ class GatewayClient:
         register_request = auth_pb2.RegisterUserRequest(username=username, password=password, email=email)
         response = self.stub.register(request=register_request)
         return response
+
+# class AuthClient:
+#     service = os.environ.get('USER_SVC_ADDRESS', 'localhost:9998')
+#
+#     def __init__(self, service=service):
+#         print(28, service)
+#         self.service = service
+#         self.channel = grpc.insecure_channel(service)
+#         self.stub = auth_pb2_grpc.AuthServiceStub(self.channel)
+#
+#     def check_username(self, username):
+#         print("AuthClient send request")
+#         request = auth_pb2.CheckUsernameRequest(username=username)
+#         return self.stub.check_username(request=request)
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, status, Response
 import schemas
 
 from client import GatewayClient
+from grpc_generated_files import auth_pb2, auth_pb2_grpc
 
 app = FastAPI()
 
@@ -16,13 +17,25 @@ def register(payload: schemas.RegisterSchema, response: Response):
         password=payload.password,
         email=payload.email)
     response.status_code = resp.status_code
-
+    print(response)
     if not resp.error:
         user = resp.success_payload
         return schemas.RegisterResponseSchema(id=user.id, username=user.username, email=user.email)
     else:
         return {'error': resp.error_payload}
+    # auth_client = AuthClient()
+    #
+    # resp = auth_client.check_username(payload.username)
+    # print(29, resp.value)
+    # if not bool(resp.value):
+    #     # user = auth_pb2.User(id=1, username=payload.username, password=payload.password, email=payload.email)
+    #     # response = auth_pb2.RegisterUserResponse(
+    #     #     success_payload=user,
+    #     #     error=False,
+    #     #     status_code=200,
+    #     # )
+    #     return {'status': 'ok'}
 
 
 if __name__ == '__main__':
-    uvicorn.run("__main__:app", host='0.0.0.0', port=8000, reload=True, workers=1)
+    uvicorn.run("__main__:app", host='0.0.0.0', port=8080, reload=True, workers=1)
